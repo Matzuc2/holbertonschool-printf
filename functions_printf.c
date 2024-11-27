fa61ee9d1a07187af352b3eb9c444f470d8b454d
Jonas-Jungling <jonas.jungling@oclock.school>
2024-11-27 14:50:53 +0100

    Added descriptions to all functions, ensured compliance with Betty coding style, and renamed the file 'functions_printf.c'.

---
+++ functions_printf.c
@@ -0,0 +1,93 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "main.h"
+#include <unistd.h>
+
+/**
+*print_int - print integer from args list
+*@args: va_list args
+*Return: the number of characters printed
+*/
+
+int print_int(va_list args)
+{
+	int count = 0;
+	int x = 1;
+	int res = va_arg(args, int);
+	int min = -2147483648;
+
+	if (res < 0)
+	{
+		_putchar('-');
+		if (res <= min)
+			res = min;
+		else
+		res *= -1;
+	}
+	while ((res / (x * 10)) > 0)
+	{
+		x *= 10;
+		count++;
+	}
+	while ((res / x) > 0)
+	{
+		_putchar((res / x) + '0');
+		res = res % x;
+		x = x / 10;
+		if (x == 0)
+		{
+			break;
+		}
+	}
+		return (count);
+}
+
+
+
+/**
+ * print_char - print character from args list
+ * @args: va_list args
+ * Return: the number of characters printed
+ */
+int print_char(va_list args)
+{
+	_putchar(va_arg(args, int));
+	return (1);
+}
+
+/**
+ * print_string - print string from args list
+ * @args: va_list args
+ * Return: the number of characters printed
+ */
+int print_string(va_list args)
+{
+	int i;
+	char *str;
+	int count = 0;
+
+
+	str = va_arg(args, char*);
+	if (str == NULL)
+	{
+		return (0);
+	}
+	for (i = 0; str[i] != '\0'; i++)
+	{
+	_putchar(str[i]);
+	count++;
+	}
+	return (count);
+}
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
--- main.h
+++ main.h
@@ -1,16 +1,34 @@
 #ifndef VARIADIC
 #define VARIADIC
 #include <stdarg.h>
+
+/**
+* struct which - Structure to define a format specifier and its corresponding
+* function
+* @w: The format specifier character (e.g., 'c' for char, 's' for string)
+* @f: Pointer to the function that handles the specified format
+*
+* Description: This structure is used to map a format specifier to its
+* corresponding
+* function in the custom printf implementation. Each format specifier character
+* is
+* associated with a function that knows how to handle and print the
+* corresponding
+* data type.
+*/
 typedef struct which
 {
-        char *w;
-        int (*f)(va_list args);
-}which_s;
+		char *w;
+
+		int (*f)(va_list args);
+} which_s;
+
 int _printf(const char *format, ...);
+
 int print_int(va_list args);
 int print_float(va_list args);
 int print_char(va_list args);
 int print_string(va_list args);
-int _pow_recursion(int x, int y);
 int _putchar(char c);
+
 #endif
