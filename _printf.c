49ec187bfee2f5de1e057346ed7cbe74fe3defe1
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2024-11-27 22:07:48 +0100

    correct the case where unknown specifier or two %% are encoutered

--- _printf.c
+++ _printf.c
@@ -29,13 +29,10 @@ int _printf(const char *format, ...)
 			{
 				count += which[j].f(args);
 				i++;
-				break;
 			}
 			else if ((which[j + 1].w) == NULL)
 			{
-				count +=  _putchar(format[i + 1]);
 				i++;
-				break;
 			}
 		}
 	}
