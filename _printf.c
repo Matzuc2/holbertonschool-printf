8fa28689973dd9718988e595550495dd59f79876
Jonas-Jungling <jonas.jungling@oclock.school>
2024-11-28 09:36:32 +0100

    Refactor with the previous version of _printf

--- _printf.c
+++ _printf.c
@@ -11,52 +11,41 @@
  */
 int _printf(const char *format, ...)
 {
-    va_list args;
-    which_s which[] = {{"d", print_int}, {"c", print_char}, {"i", print_int},
-                       {"s", print_string}, {"%", is_percent}, {NULL, NULL}};
-    unsigned int i = 0, j = 0, count = 0;
+	va_list args;
+	which_s which[] = {{"d", print_int}, {"c", print_char}, {"i", print_int},
+	{"s", print_string}, {"%", is_percent}, {NULL, NULL}};
+	unsigned int i = 0, j = 0, count = 0;
 
-    if (format == NULL)
-        return (-1); // Vérifie si format est NULL
-
-    va_start(args, format);
-
-    while (format[i] != '\0')
-    {
-        if (format[i] == '%') // Détecte un spécificateur
-        {
-            if (format[i + 1] == '\0') // Si % est le dernier caractère
-            {
-                va_end(args);
-                return (-1); // Retourne une erreur
-            }
-
-            for (j = 0; which[j].w != NULL; j++)
-            {
-                if (format[i + 1] == *(which[j].w)) // Spécificateur trouvé
-                {
-                    count += which[j].f(args);
-                    i++; // Ignore le caractère suivant '%'
-                    break;
-                }
-            }
-
-            if (which[j].w == NULL) // Aucun spécificateur correspondant trouvé
-            {
-                count += _putchar('%');    // Affiche le '%'
-                count += _putchar(format[i + 1]); // Affiche le caractère inconnu
-                i++; // Ignore le caractère suivant '%'
-            }
-        }
-        else
-        {
-            count += _putchar(format[i]); // Affiche un caractère normal
-        }
-        i++;
-    }
-
-    va_end(args);
-    return (count);
+	va_start(args, format);
+	if (format == NULL)
+		return (-1);
+	for (; format && format[i] != '\0'; i++)
+	{
+	j = 0;
+	if (format[i] == '%')
+	{
+		for (; which[j].w != NULL; j++)
+		{
+			if (format[i + 1] == *(which[j].w))
+			{
+				count += which[j].f(args);
+				i++;
+				break;
+			}
+			else if ((which[j + 1].w) == NULL)
+			{
+				count += _putchar(format[i]) + _putchar(format[i + 1]);
+				i++;
+				break;
+			}
+		}
+	}
+	else
+	{
+		count += _putchar(format[i]);
+	}
+	}
+	va_end(args);
+	return (count);
 }
-
 
